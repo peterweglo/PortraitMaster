99cdd68385b165a068b8dd62b72b4bc94104d5bb
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-02-28 21:22:20 +0100

    Add security

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -3,54 +3,70 @@ const Photo = require('../models/photo.model');
 /****** SUBMIT PHOTO ********/
 
 exports.add = async (req, res) => {
+  const escape = (html) => {
+    return html
+      .replace(/&/g, '')
+      .replace(/</g, '')
+      .replace(/>/g, '')
+      .replace(/"/g, '')
+      .replace(/'/g, '');
+  };
 
   try {
-    const { title, author, email } = req.fields;
+    let { title, author, email } = req.fields;
+    title = escape(title);
+    author = escape(author);
+    email = escape(email);
     const file = req.files.file;
 
-    if(title && author && email && file) { // if fields are not empty...
+    if (title && author && email && file) {
+      // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
-      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-      await newPhoto.save(); // ...save new photo in DB
-      res.json(newPhoto);
-
+      const fileExt = fileName.split('.').slice(-1)[0];
+      if (fileExt === 'jpg' || fileExt === 'gif' || fileExt === 'png') {
+        const newPhoto = new Photo({
+          title,
+          author,
+          email,
+          src: fileName,
+          votes: 0,
+        });
+        await newPhoto.save(); // ...save new photo in DB
+        res.json(newPhoto);
+      } else {
+        throw new Error('Wrong input!!');
+      }
     } else {
       throw new Error('Wrong input!');
     }
-
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** LOAD ALL PHOTOS ********/
 
 exports.loadAll = async (req, res) => {
-
   try {
     res.json(await Photo.find());
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
 
 /****** VOTE FOR PHOTO ********/
 
 exports.vote = async (req, res) => {
-
   try {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
+    if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-
 };
--- models/Photo.model.js
+++ models/Photo.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const photoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
+  title: { type: String, maxLength: 25, required: true },
+  author: { type: String, maxLength: 50, required: true },
   email: { type: String, required: true },
   src: { type: String, required: true },
   votes: { type: Number, required: true },
